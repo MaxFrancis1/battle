d8f3a1af40a2cddd05b758433b3f5bbe9af91050
Max <max.francis2000@gmail.com>
2018-10-17 13:54:41 +0000

    refactoring

--- app.rb
+++ app.rb
@@ -2,16 +2,26 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
-  end
+  enable :sessions
   
   get '/' do
     erb :index
   end
-
+  
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name] 
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+  
+  
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    p session
+    erb :play
+  end
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
