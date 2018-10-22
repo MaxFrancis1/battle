b4ebe311397bc2f327cacf86ad0bfa7138aabc55
Max <max.francis2000@gmail.com>
2018-10-22 09:14:06 +0000

    Made it a multiplayer game

--- app.rb
+++ app.rb
@@ -3,13 +3,12 @@ require_relative ('./lib/player.rb')
 require_relative ('./lib/game.rb')
 
 class Battle < Sinatra::Base
-  
   enable :sessions
-  
+
   get '/' do
     erb :index
   end
-  
+
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
@@ -21,14 +20,18 @@ class Battle < Sinatra::Base
     @game = $game
     erb :play
   end
-  
+
   get '/attack' do
     @game = $game
-    @game.attack(@game.player_2)
-    @game.switch_turns
+    @game.attack(@game.opponent_of(@game.current_turn))
     erb :attack
   end
-  
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
